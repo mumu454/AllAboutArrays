048820a47768d555161ac24f25ec7b8c94a52fb4
Muna Chimoriya <muna.chimoriya.2022student.lhschools.org>
2020-05-20 01:18:32 -0400

    pattern4 Completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -63,4 +63,10 @@ public class Main {
         }
 
 
-}
+        public static void Pattern4(){
+        for(int i =1; i <= 6;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
